cfaf9f827f0cc3344adc7db9540a0af3cdba0096
Thomas Mauricio Rodriguez <thomasmauricio521@gmail.com>
2023-05-13 19:50:48 -0500

    Tal vez no es importante

--- pages/PartidosCreados.js
+++ pages/PartidosCreados.js
@@ -1,7 +1,22 @@
 const PartidosCreados = () => {
-    return`
-    <div>HOLA</div>
-    `
+return`
+<div class="contenedorPartidos">\
+    <div class="container">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque quaerat quibusdam assumenda voluptas
+            iure est similique nulla, laborum quam perferendis, mollitia debitis sint incidunt id ipsam inventore,
+            nostrum sed commodi.</p>
+    </div>
+
+</div>
+
+<style>
+    .contenedorPartidos {
+        padding: 2%;
+        background-color: white;
+        height: 80vh;
+    }
+</style>
+`
 }
 
 export { PartidosCreados }
--- pages/Registrarse.js
+++ pages/Registrarse.js
@@ -1,5 +1,54 @@
 const Registrarse = () => {
+return `
+<div id="contenedorRegister">
+    <div class="row p-5 cardQuery mx-auto ">
+        <div class="col-auto card rounded-0 card-body  p-4">
+            <div class=" mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="text" class="form-control" placeholder="name@example.com" required>
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="text" class="form-control" placeholder="name@example.com" required>
+            </div>
+        </div>
+    </div>
+</div>
 
+<style>
+    #contenedorRegister {
+        background-color: white;
+        height: 80vh;
+    }
+
+    @media (max-width:100%) or (min-width:800px) {
+        .cardQuery {
+            width: 50%;
+            text-align: justify;
+        }
+    }
+
+    @media (max-width: 800px) or (min-width:430) {
+        .cardQuery {
+            width: auto;
+            text-align: justify;
+        }
+    }
+
+    @media (max-width:430px) {
+        .cardQuery {
+            width: 100%;
+            text-align: justify;
+        }
+
+        #contenedorRegister {
+            background-color: white;
+            /* height: 20vh; */
+        }
+    }
+</style>
+
+`
 }
 
 export {Registrarse}
